dfd25ff4dcae3bfba3aec05d7c9d8c2fae401439
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-06-15 17:13:35 +0530

    Implement JWT-based user authentication

---
+++ Backend/controllers/jwt/tokengenration.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const createTokenAndSaveCokkie = (userId , res) =>{
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '5d' });
+    res.cookie('jwt', token, { httpOnly: true , secure: true, maxAge: 77700000 });
+}
+
+export default createTokenAndSaveCokkie;
--- Backend/controllers/user_controller.js
+++ Backend/controllers/user_controller.js
@@ -1,5 +1,6 @@
 import user from '../models/user_model.js';
 import bcrypt from "bcryptjs";
+import createTokenAndSaveCokkie from './jwt/tokengenration.js';
 
 export const signup = async (req, res) => {
     try {
@@ -18,7 +19,10 @@ export const signup = async (req, res) => {
             password: hashedPassword,
         });
         await newUser.save();
-        res.status(201).json({ message: "User created successfully" });
+        if(newUser){
+            createTokenAndSaveCokkie(newUser._id , res);
+            res.status(201).json({ message: "User created successfully" , newUser });
+        }
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
