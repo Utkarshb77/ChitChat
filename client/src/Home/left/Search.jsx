ff33fe97b47341749d5f5478f396d56cd0387927
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-06-30 16:21:34 +0530

    Search functinality done

--- client/src/Home/left/Search.jsx
+++ client/src/Home/left/Search.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoSearchCircleOutline } from "react-icons/io5";
+import useGetAllUsers from '../../context/userGetAllUsers.jsx';
+import useconversations from '../../statemanage/useconversation.js';
 
 function Search() {
+    const [search, setSearch] = useState('');
+    const { allUsers } = useGetAllUsers();
+    const { setSelectedConversation } = useconversations();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!search.trim()) return;
+        const conversation = allUsers.find((user) => user.fullname.toLowerCase().includes(search.toLowerCase()));
+        if (conversation) {
+            setSelectedConversation(conversation);
+            setSearch('');
+        } else {
+            alert("No user found");
+        }
+    }
+
     return (
-            <div className='px-6 py-4'>
-            <form action='' method=''>
+        <div className='px-6 py-4'>
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <div className='flex space-x-2' >
-                <label className="input flex items-center gap-2 w-[80%]">
-                    <input type="search" className="grow" placeholder="Search" />
-                </label>
-                <button>
-                    <IoSearchCircleOutline className='text-5xl p-2 hover:bg-gray-600 rounded-full duration-300'/>
-                </button>
+                    <label className="input flex items-center gap-2 w-[80%]">
+                        <input type="search" className="grow" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+                    </label>
+                    <button>
+                        <IoSearchCircleOutline className='text-5xl p-2 hover:bg-gray-600 rounded-full duration-300' />
+                    </button>
                 </div>
             </form>
         </div>
