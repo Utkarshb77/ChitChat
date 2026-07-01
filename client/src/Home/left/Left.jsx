e055cfd25ca7b0d18e0ae8424f8271125960684d
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-07-01 16:52:24 +0530

    Update logo to dark version, remove logout icon from sidebar

--- client/src/Home/left/Left.jsx
+++ client/src/Home/left/Left.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Search from './Search';
 import Users from './Users';
-import { MdLogout } from "react-icons/md";
+
 import axios from 'axios';
 import { useAuth } from '../../context/AuthProvider';
 import toast from 'react-hot-toast';
@@ -22,11 +22,8 @@ function Chats() {
 
   return (
     <div className='w-full bg-black text-white flex flex-col h-full'>
-      <div className='flex items-center justify-between p-2 px-4 md:px-5'>
+      <div className='flex items-center p-2 px-4 md:px-5'>
         <img src='/logo.png' alt='ChitChat' className='h-12 w-auto object-contain' />
-        <button onClick={handleLogout} className='md:hidden'>
-          <MdLogout className='text-4xl p-1.5 hover:bg-gray-600 rounded-lg duration-300' />
-        </button>
       </div>
       <Search />
       <hr></hr>
